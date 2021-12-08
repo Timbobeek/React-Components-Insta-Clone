f85028ee917d7c9ff3437111d20d35aab1fc39b8
Tim Goloshchapov <tim.goloschapov@gmail.com>
2021-12-07 22:26:22 -0600

    some keys added + comments

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ const App = () => {
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
   const [posts, setPosts] = useState(dummyData)
 
-  const likePost = postId => {
+  const likePost = likedPostId => {
     /*
       This function serves the purpose of increasing the number of likes by one, of the post with a given id.
 
@@ -27,23 +27,30 @@ const App = () => {
       This function is passed down to nested components through props, allowing them to increase the number of likes of a given post.
       Invoke `setPosts` and pass as the new state the invocation of `posts.map`.
       The callback passed into `map` performs the following logic:
-        - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
+        - if the `id` of the post matches `likedPostId`, return a new post object with the desired values (use the spread operator).
         - otherwise just return the post object unchanged.
      */
-    setPosts(posts.map(post => {
-      if (post.id === postId){
+
+    // [a, b, c] ---> [a, b', c]  this what we want .map to achieve
+   
+    const newState = posts.map(post => {
+      if (post.id === likedPostId){
         return {...post, likes: post.likes + 1}
+        //'...' above takes all key/value pairs out of a post, and transfers them into a newly created object, BUT changes the value of 'likes' to 'post.likes + 1'
       } else {
-        return post;
+        return post; // every time a post is liked, we change its state, but for all other posts that exist and weren't liked we need them to be displayed still, in their default state (this is what else part is for) 
       }
-    }));
+    });
+
+    setPosts(newState);
   };
 
   return (
     <div className='App'>
       {/* Add SearchBar and Posts here to render them */}
       <SearchBar/>
-      <Posts likePost={likePost} posts={posts}/>
+      <Posts likePost={likePost} posts={posts}/> 
+      {/* first posts (prop of Posts, Posts.props.posts) comes from posts.js       and second posts (current state) is from app.js */}
       {/* Check the implementation of each component, to see what props they require, if any! */}
     </div>
   );
